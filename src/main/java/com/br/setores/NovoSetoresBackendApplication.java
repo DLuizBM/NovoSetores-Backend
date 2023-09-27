130a4c30a539ae9354669e51995196c6e821f06e
Divino Luiz <divino.moreira@foton.la>
2023-09-27 00:09:46 -0300

    initial config

---
+++ src/main/java/com/br/setores/NovoSetoresBackendApplication.java
@@ -0,0 +1,13 @@
+package com.br.setores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovoSetoresBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovoSetoresBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/setores/NovoSetoresBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.setores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovoSetoresBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
